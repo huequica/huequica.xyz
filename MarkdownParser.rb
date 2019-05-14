8f11bfcd47489a50c0c3285d7bc5e3cb1d723b9f
huequica <harumaki1225@gmail.com>
2019-05-14 19:46:05 +0900

    add HTML sheets

--- MarkdownParser.rb
+++ MarkdownParser.rb
@@ -1,12 +1,29 @@
 require 'redcarpet'
+require 'json'
 
 Markdown_Parser = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-md = '
+md = Markdown_Parser.render('
 # Hello world
 
 ## ようこそredcarpetへ
 
 + 便利そう？
 + クラス割当は自分で実装しないといけなさそう
-'
-puts Markdown_Parser.render(md)
+')
+# puts Markdown_Parser.render(md)
+
+HTML = "<!DOCTYPE HTML>
+<html>
+<head>
+  <title>test</title>
+  <link rel=\"stylesheet\" href=\"css/bulma.css\">
+  <meta charset='utf-8'>
+</head>
+
+<body>
+#{md}
+</body>
+</html>
+".freeze
+
+puts HTML
