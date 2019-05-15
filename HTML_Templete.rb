5bf6009b0da3fefeb7e58b1bc79ae254b7f4c27d
huequica <harumaki1225@gmail.com>
2019-05-15 09:37:10 +0900

    Move HTML Data

---
+++ HTML_Templete.rb
@@ -0,0 +1,13 @@
+HTML = "<!DOCTYPE HTML>
+<html>
+<head>
+  <title>test</title>
+  <link rel=\"stylesheet\" href=\"css/bulma.css\">
+  <meta charset='utf-8'>
+</head>
+
+<body>
+#{md}
+</body>
+</html>
+".freeze
--- MarkdownParser.rb
+++ MarkdownParser.rb
@@ -32,18 +32,4 @@ File.open(FILENAME, 'r') do |f|
   md = markdown_parse(f.read)
 end
 
-HTML = "<!DOCTYPE HTML>
-<html>
-<head>
-  <title>test</title>
-  <link rel=\"stylesheet\" href=\"css/bulma.css\">
-  <meta charset='utf-8'>
-</head>
-
-<body>
-#{md}
-</body>
-</html>
-".freeze
-
 file_write(HTML)
